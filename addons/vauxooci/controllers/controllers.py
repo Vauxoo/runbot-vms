c9d49934ee84ad272ceedf12ceac3829f8ba6393
Nhomar Hernández <nhomar@vauxoo.com>
2016-05-10 21:12:43 -0500

    Rebase- Button just started work.

--- addons/vauxooci/controllers/controllers.py
+++ addons/vauxooci/controllers/controllers.py
@@ -1,10 +1,55 @@
 # -*- coding: utf-8 -*-
 from openerp import http
+from openerp.http import request
 
-# class RunbotFrontend(http.Controller):
-#     @http.route('/runbot_frontend/runbot_frontend/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+
+def s2human(time):
+    """Convert a time in second into an human readable string"""
+    for delay, desc in [(86400,'d'),(3600,'h'),(60,'m')]:
+        if time >= delay:
+            return str(int(time / delay)) + desc
+    return str(int(time)) + "s"
+
+
+class RunbotButtons(http.Controller):
+
+    def build_info(self, build):
+        real_build = build.duplicate_id if build.state == 'duplicate' else build
+        return {
+            'id': build.id,
+            'name': build.name,
+            'state': real_build.state,
+            'result': real_build.result,
+            'subject': build.subject,
+            'author': build.author,
+            'committer': build.committer,
+            'dest': build.dest,
+            'real_dest': real_build.dest,
+            'job_age': s2human(real_build.job_age),
+            'job_time': s2human(real_build.job_time),
+            'job': real_build.job,
+            'domain': real_build.domain,
+            'host': real_build.host,
+            'port': real_build.port,
+            'subject': build.subject,
+            'server_match': real_build.server_match,
+        }
+
+    @http.route(['/vauxooci/build_button/<build_id>'], type='http', auth="public", website=True)
+    def build(self, build_id=None, search=None, **post):
+        registry, cr, uid, context = request.registry, request.cr, request.uid, request.context
+
+        Build = registry['runbot.build']
+
+        build = Build.browse(cr, uid, [int(build_id)])[0]
+        if not build.exists():
+            return request.not_found()
+        context = {
+            'repo': build.repo_id,
+            'bu': self.build_info(build),
+            'br': {'branch': build.branch_id},
+        }
+        return request.render("vauxooci.build_button", context)
 
 #     @http.route('/runbot_frontend/runbot_frontend/objects/', auth='public')
 #     def list(self, **kw):
--- addons/vauxooci/static/src/js/vauxooci.js
+++ addons/vauxooci/static/src/js/vauxooci.js
@@ -3,13 +3,26 @@
 
     $(function() {
         $('.vauxooci-display').click(function() {
-            console.log( $(this).data('target-id'));
 
             if ( $('#'+$(this).data('target-id')).hasClass( "hidden"  )  ) {
                 $('#'+$(this).data('target-id')).removeClass('hidden');
                 return true
             }
             $('#'+$(this).data('target-id')).addClass('hidden');
+        });
+        $('.build-card').click(function() {
+            var self = $(this);
+            $.ajax({
+                url: "/vauxooci/build_button/"+self.data('build-id'),
+                context: document.body
+            }).done(function(data) {
+                    var res = $(data).css({"position": "fixed",
+                                 "top": 0,
+                                 "left": 0,
+                             }).data('build-id',
+                                     self.data('build-id'));
+                    res.appendTo($('body'));
+            });
        });
     });
 
